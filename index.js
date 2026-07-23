731bef72ab12620433cd5db84b8e7e8de28a7bd7
Parvathi73 <your-email@example.com>
2026-07-23 22:09:10 +0530

    Keep only required project files

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import CoreConnect from './CoreConnect';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => CoreConnect);
