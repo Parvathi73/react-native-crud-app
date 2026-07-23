f68df59f59836fd65651a22359359b5be33d441a
Parvathi73 <your-email@example.com>
2026-07-23 22:06:13 +0530

    Remove unnecessary files

--- __tests__/CoreConnect.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import CoreConnect from '../CoreConnect';
-
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<CoreConnect />);
-  });
-});
